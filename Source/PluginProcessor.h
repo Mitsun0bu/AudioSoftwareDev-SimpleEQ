f96a0c6d9fddf0ad2d7884ca0f93d958d133fbc2
Lucas Lethuillier <lucas.lethuillier@gmail.com>
2023-05-06 15:45:42 +0200

    feature(lowCut-filter): refactor the way lowCut filter is updated with creation of an updateCutFilter() method

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -80,7 +80,12 @@ public:
     static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
   
     // [LUCAS] : This is the object that manages the parameters of my EQ
-    juce::AudioProcessorValueTreeState parametersManager{*this, nullptr, "Parameters", createParameterLayout()};
+    juce::AudioProcessorValueTreeState parametersManager{
+        *this,
+        nullptr,
+        "Parameters",
+        createParameterLayout()
+    };
 
 private:
 
@@ -111,6 +116,59 @@ private:
     using Coefficients = Filter::CoefficientsPtr;
     static void updateCoefficients(Coefficients& old, const Coefficients& updated);
 
+    // [LUCAS] :
+    template<typename ChainType, typename CoefficientType>
+    void updateCutFilter(
+        ChainType& leftLowCut,
+        const CoefficientType& cutCoefficients,
+        const Slope& lowCutSlope)
+    {
+        leftLowCut.template setBypassed<0>(true);
+        leftLowCut.template setBypassed<1>(true);
+        leftLowCut.template setBypassed<2>(true);
+        leftLowCut.template setBypassed<3>(true);
+
+        // [LUCAS] : 
+        switch (lowCutSlope)
+        {
+            case Slope_12:
+            {
+                *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+                leftLowCut.template setBypassed<0>(false);
+                break;
+            }
+            case Slope_24:
+            {
+                *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+                leftLowCut.template setBypassed<0>(false);
+                *leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
+                leftLowCut.template setBypassed<1>(false);
+                break;
+            }
+            case Slope_36:
+            {
+                *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+                leftLowCut.template setBypassed<0>(false);
+                *leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
+                leftLowCut.template setBypassed<1>(false);
+                *leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
+                leftLowCut.template setBypassed<2>(false);
+                break;
+            }
+            case Slope_48:
+            {
+                *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+                leftLowCut.template setBypassed<0>(false);
+                *leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
+                leftLowCut.template setBypassed<1>(false);
+                *leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
+                leftLowCut.template setBypassed<2>(false);
+                *leftLowCut.template get<3>().coefficients = *cutCoefficients[3];
+                leftLowCut.template setBypassed<3>(false);
+                break;
+            }
+        }
+    }
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
 };
